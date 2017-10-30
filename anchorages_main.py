12649d1b7b0585eddd326c269c066dd9e5491825
bitsofbits <tim@bitsofbits.com>
2017-10-30 15:33:03 -0700

    fix examples

--- anchorages_main.py
+++ anchorages_main.py
@@ -3,7 +3,7 @@
     Minimal dataflow run:
 
         python -m anchorages_main \
-            --name test-anchorages-2016-tiny \
+            --name testanchorages2016tiny \
             --start-date 2016-01-01 \
             --end-date 2016-01-31 \
 
@@ -11,14 +11,14 @@
     2016 dataflow run:
 
         python -m anchorages_main \
-            --name test-anchorages-2016 \
+            --name testanchorages2016 \
             --start-date 2016-01-01 \
             --end-date 2016-12-31 \
 
     Full dataflow run:
 
         python -m anchorages_main \
-            --name test-anchorages-all-years \
+            --name testanchoragesallyears \
             --start-date 2012-01-01 \
             --end-date 2017-12-31 \
 """
