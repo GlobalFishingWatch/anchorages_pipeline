df9a147dac505be2078d214a39bf1d8c63708164
bitsofbits <tim@bitsofbits.com>
2017-11-05 15:05:01 -0700

    update example

--- port_visits_main.py
+++ port_visits_main.py
@@ -2,11 +2,11 @@
 
     Minimal run:
 
-        python -m port_visits_main --name testvisits \
+        python -m port_visits_main --name portvisits_time_one_day \
                                    --anchorages gfw_raw.anchorage_naming_20171026 \
-                                   --start-date 2016-02-01 \
-                                   --end-date 2016-02-01 \
-                                   --max_num_workers 50
+                                   --start-date 2016-01-01 \
+                                   --end-date 2016-01-01 \
+                                   --max_num_workers 100
             
 """
 import logging
