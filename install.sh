0b0aa1990313291f1462793972604d0f442a69aa
bitsofbits <tim@bitsofbits.com>
2017-11-30 09:31:11 -0700

    fix dag name again

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 echo "Installing dags"
-cp ./port_events_day.py /dags/port_events_dag.py
+cp ./port_events_dag.py /dags/port_events_dag.py
 echo "Installing post_install.sh"
 cp ./post_install.sh /dags/post_install.sh
