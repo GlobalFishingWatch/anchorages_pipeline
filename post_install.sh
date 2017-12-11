e8df6363233aed150e73241fcce712f04a06533f
bitsofbits <tim@bitsofbits.com>
2017-12-11 09:18:28 -0700

    fix post install

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,6 @@
-python $AIRFLOW_HOME/utils/set_default_variables.py PIPE_ANCHORAGES \
+python $AIRFLOW_HOME/utils/set_default_variables.py \
     --force DOCKER_IMAGE=gcr.io/world-fishing-827/$1 \
+    PIPE_ANCHORAGES \
     PORT_EVENTS_ANCHORAGE_TABLE=gfw_raw.anchorage_naming_20171026 \
     PORT_EVENTS_INPUT_TABLE=pipeline_classify_p_p516_daily \
     PORT_EVENTS_OUTPUT_TABLE=machine_learning_dev_ttl_30d.in_out_events_test \
