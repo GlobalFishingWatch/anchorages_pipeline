f5d461e0616b64d5fbe53ec64075ea1b037eb780
bitsofbits <tim@bitsofbits.com>
2017-12-11 08:23:26 -0700

    add force for DOCKER_IMAGE

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,8 @@
 python $AIRFLOW_HOME/utils/set_default_variables.py PIPE_ANCHORAGES \
+    --force DOCKER_IMAGE=gcr.io/world-fishing-827/$1 \
     PORT_EVENTS_ANCHORAGE_TABLE=gfw_raw.anchorage_naming_20171026 \
     PORT_EVENTS_INPUT_TABLE=pipeline_classify_p_p516_daily \
     PORT_EVENTS_OUTPUT_TABLE=machine_learning_dev_ttl_30d.in_out_events_test \
-    GCS_BUCKET=machine-learning-dev-ttl-30d/anchorages \
-    DOCKER_IMAGE=gcr.io/world-fishing-827/$1
+    GCS_BUCKET=machine-learning-dev-ttl-30d/anchorages
 
 echo "Installation Complete"
