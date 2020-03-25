6f1432e621c3b7bfc3737c2cb413ba80ff24c92b
Enrique Tuya <enrique@globalfishingwatch.org>
2020-03-25 13:50:03 -0300

    Change the print script to support python3.

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PIPELINE='pipe_anchorages'
-PIPELINE_VERSION=$(python -c "import pkg_resources; print pkg_resources.get_distribution('${PIPELINE}').version")
+PIPELINE_VERSION=$(python -c "import pkg_resources; print(pkg_resources.get_distribution('${PIPELINE}').version)")
