9ec1e910396006ce1001f5cbc0549006b4f58cb2
Matias Piano <matias@globalfishingwatch.org>
2021-09-15 23:58:53 -0300

    Adds the wait_for_job option in port_events and port_visits needed to sync processes

--- pipe_anchorages/options/port_events_options.py
+++ pipe_anchorages/options/port_events_options.py
@@ -10,7 +10,7 @@ class PortEventsOptions(PipelineOptions):
         required = parser.add_argument_group('Required')
         optional = parser.add_argument_group('Optional')
 
-        required.add_argument('--anchorage_table', 
+        required.add_argument('--anchorage_table',
                             help='Name of of anchorages table (BQ)')
         required.add_argument('--input_table', required=True,
                             help='Table to pull position messages from')
@@ -18,14 +18,16 @@ class PortEventsOptions(PipelineOptions):
                             help='Table containing port state on each day')
         required.add_argument('--output_table', required=True,
                             help='Output table (BQ) to write results to.')
-        required.add_argument('--start_date', required=True, 
+        required.add_argument('--start_date', required=True,
                               help="First date to look for entry/exit events.")
-        required.add_argument('--end_date', required=True, 
+        required.add_argument('--end_date', required=True,
                             help="Last date (inclusive) to look for entry/exit events.")
-        
+
         optional.add_argument('--config', default='anchorage_cfg.yaml',
                             help="Path to configuration file")
-        optional.add_argument('--ssvid_filter', 
+        optional.add_argument('--ssvid_filter',
                             help='Subquery or list of ssvid to limit processing to.\n'
                                  'If prefixed by @, load from given path')
-   
+        optional.add_argument('--wait_for_job', default=False, action='store_true',
+                            help='Wait until the job finishes before returning.')
+
