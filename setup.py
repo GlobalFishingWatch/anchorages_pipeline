9c7ea456ebed6a24abe7069cf842c050fa9d7550
Matias Piano <matias@globalfishingwatch.org>
2023-08-14 17:12:24 -0300

    Adds support for AB-2.49

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pipe_anchorages",
-    version="4.0.0",
+    version="4.1.0",
     packages=find_packages(exclude=["test*.*", "tests"]),
     package_data={
         "": ["data/port_lists/*.csv", "data/EEZ/*"],
