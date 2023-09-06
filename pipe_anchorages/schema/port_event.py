2c738408a21ff2e994931d350c1e5dd859a071f3
Matias Piano <matias@globalfishingwatch.org>
2023-09-06 15:28:30 -0300

    Removes events.ssvid and events.last_timestamp to avoid confusion with the implementation

--- pipe_anchorages/schema/port_event.py
+++ pipe_anchorages/schema/port_event.py
@@ -5,7 +5,6 @@ def build():
 
     builder = SchemaBuilder()
 
-    builder.add("ssvid", "STRING", description="The ssvid of the vessel involved in the port.")
     builder.add("seg_id", "STRING", description="The segment id belonging to the vessel.")
     builder.add("timestamp", "TIMESTAMP", description="The timestamp when the message was received.")
     builder.add("lat", "FLOAT", description="The latitude included in the message.")
@@ -14,6 +13,5 @@ def build():
     builder.add("vessel_lon", "FLOAT", mode="NULLABLE", description="The longitude of the vessel.")
     builder.add("anchorage_id", "STRING", description="The id of the anchorage.")
     builder.add("event_type", "STRING", description="The event type.")
-    builder.add("last_timestamp", "TIMESTAMP", mode="NULLABLE", description="The last timestamp")
 
     return builder.schema
