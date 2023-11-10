c2378b31d57df6ad9203a38a7c9d2238660fd553
Andres Arana <andres@globalfishingwatch.org>
2023-11-10 15:14:36 -0300

    Removes read source multiplexing as it's not really needed

--- pipe_anchorages/transforms/voyages_read_source.py
+++ pipe_anchorages/transforms/voyages_read_source.py
@@ -17,41 +17,32 @@ SOURCE_QUERY_TEMPLATE = """
       `{source_table}`
     WHERE
         date(end_timestamp) >= "{start:%Y-%m-%d}"
-        AND date(end_timestamp) <= "{end:%Y-%m-%d}"
         AND confidence >= 2
 
 """
-    # ORDER BY end_timestamp, vessel_id
-    # LIMIT 1000
+
+def cloud_options_to_labels(options):
+    return dict([x.split('=') for x in options.labels])
 
 class ReadSource(beam.PTransform):
-    def __init__(self, source_table, first_table_date, cloud):
+    def __init__(self, source_table, first_table_date, cloud_options):
         self.first_table_date = dt.datetime.strptime(first_table_date,'%Y-%m-%d')
         self.source_table = source_table
-        cloud_to_labels = lambda ll: {x.split('=')[0]:x.split('=')[1] for x in ll}
-        self.labels = cloud_to_labels(cloud.labels)
+        self.labels = cloud_options_to_labels(cloud_options)
 
     def expand(self, pcoll):
         return (
-            [
-                pcoll
-                | f"Read_visits_{i}" >> beam.io.ReadFromBigQuery(query=query, use_standard_sql=True, bigquery_job_labels=self.labels)
-                for i, query in enumerate(self.read_source())
-            ]
-            | beam.Flatten()
+            pcoll
+            | self.read_source()
         )
 
     def read_source(self):
-        start = self.first_table_date
-        end = dt.datetime.now()
-        while start <= end:
-            next_start = start + dt.timedelta(days=365)
-            min_end = min(next_start - dt.timedelta(days=1), end)
-
-            yield SOURCE_QUERY_TEMPLATE.format(
-                source_table=self.source_table,
-                start=start,
-                end=min_end,
-            )
-            start = next_start
-
+        query = SOURCE_QUERY_TEMPLATE.format(
+            source_table=self.source_table,
+            start=self.first_table_date
+        )
+        return beam.io.ReadFromBigQuery(
+            query=query,
+            use_standard_sql=True,
+            bigquery_job_labels=self.labels
+        )
