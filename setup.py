e2bae2228d163e9497fffbb705803bc5bb4290b0
Matias Piano <matias@globalfishingwatch.org>
2023-11-14 16:59:38 -0300

    Fixes the voyages trip_id and vessel_id description column

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pipe_anchorages",
-    version="4.2.0",
+    version="4.2.1",
     packages=find_packages(exclude=["test*.*", "tests"]),
     package_data={
         "": ["data/port_lists/*.csv", "data/EEZ/*"],
