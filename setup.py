3686d09fe08217e5039b2b64a1c499d20ad13e48
Matias Piano <matias@globalfishingwatch.org>
2023-11-28 18:31:33 -0300

    Adds voyages calculation using voyages simplified BQ queries

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="pipe_anchorages",
-    version="4.2.1",
+    version="4.2.2",
     packages=find_packages(exclude=["test*.*", "tests"]),
     package_data={
         "": ["data/port_lists/*.csv", "data/EEZ/*"],
